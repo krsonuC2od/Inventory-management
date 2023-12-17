48d4a22fcb10ccc78f73e7c9178ea91f37f7923a
krsonuC2od <190101120062@cutm.ac.in>
2023-12-17 13:44:21 +0530

    static file sended on browser throw controller

--- index.js
+++ index.js
@@ -1,10 +1,25 @@
-const express  = require('express')
+// const express  = require('express')
+import express from 'express';
+import ProductController from './src/Controllers/product.controller.js';
 const Port =3100;
 const app = express();
+ // instance of productController for use class product controller
+const productController = new ProductController();
+//default req
+app.use('/',productController.getProducts)
 
-app.get('/',(req,res)=>{
-    res.send("welcome to home");
-})
+// Middleware for use static file
+app.use(express.static('src/Views'));
+
+
+
+
+
+
+
+
+
+// server running 
 app.listen(Port,()=>{
     console.log(`your server is running on ${Port}`);
 })
